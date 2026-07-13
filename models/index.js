3cfd62446663261a8326e4d47a7d0c4f9b95a20c
NICOGATT <nicogatt1@hotmail.com>
2026-07-13 09:35:04 -0300

    fix: Borre unos los archivos de las migraciones, y el index.js de models que extraiga los modelos, pero como empezamos con mongodb no se utilizaban

--- models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Usuario: require('./usuario'),
-  Proyecto: require('./proyecto'),
-  Tecnologia: require('./tecnologia'),
-  ProjectImage: require('./projectimage'),
-  projectImage: require('./projectimage'),
-  ProyectoTecnologia: require('./proyectotecnologia'),
-  Contact: require('./contact')
-};
