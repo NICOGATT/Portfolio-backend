9f64f6b8224f7bfe6900cba8775e3a960db56422
NICOGATT <nicogatt1@hotmail.com>
2026-07-24 12:02:46 -0300

    feat: format date in getVisits response for improved readability

--- services/analytics.service.js
+++ services/analytics.service.js
@@ -69,8 +69,10 @@ async function getVisits(startDate = "30daysAgo", endDate = "today") {
         ]
     })
 
+    const rawDate = row.dimensionValues[0].value; 
+    const date = `${rawDate.slice(0, 4)}-${rawDate.slice(4, 6)}-${rawDate.slice(6, 8)}`
     return response.rows.map((row) => ({
-        date : row.dimensionValues[0].value, 
+        date, 
         users : Number(row.metricValues[0].value)
     }))
 }
