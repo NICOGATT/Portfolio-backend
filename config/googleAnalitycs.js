b153405bfc14c00118a893629b8179681ffbf61a
NICOGATT <nicogatt1@hotmail.com>
2026-07-24 11:11:10 -0300

    feat: initialize Google Analytics Data API client configuration

--- config/googleAnalitycs.js
+++ config/googleAnalitycs.js
@@ -4,16 +4,22 @@ const propertyId = process.env.GA_PROPERTY_ID;
 const clientEmail = process.env.GA_CLIENT_EMAIL; 
 const privateKey = process.env.GA_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
-if(!propertyId || !clientEmail || !privateKey) {
-    throw new Error(
-        'Faltan GA_PROPERTY_ID, GA_CLIENT_EMAIL o GA_PRIVATE_KEY'
-    )
+if (!propertyId) {
+  throw new Error('Falta GA_PROPERTY_ID');
+}
+
+if (!clientEmail) {
+  throw new Error('Falta GA_CLIENT_EMAIL');
+}
+
+if (!privateKey) {
+  throw new Error('Falta GA_PRIVATE_KEY');
 }
 
 const analyticsDataClient = new BetaAnalyticsDataClient({
     credentials: {
-        clientEmail,
-        privateKey,
+        clientEmail : clientEmail,
+        privateKey : privateKey,
     },
 });
 
